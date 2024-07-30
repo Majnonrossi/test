72b82df904a6a3b295152a139436d4414731b05f
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-07-30 15:42:42 +0100

    Create تست.js

---
+++ plugins/تست.js
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `*هذا الأمر خاص بتوليد الصور باستعمال الذكاء الإصطناعي*\n\n*مثال*\n*${usedPrefix + command} boy*`;
+
+  try {
+    m.reply('*الرجاء الانتظار، جاري إنشاء الصور...*');
+
+    const endpoint = `https://api.bk9.site/ai/photoleap?q=${encodeURIComponent(text)}`;
+    const response = await fetch(endpoint);
+
+    if (response.ok) {
+      const imageBuffer = await response.buffer();
+      await conn.sendFile(m.chat, imageBuffer, 'image.png', null, m);
+    } else {
+      throw '*فشل إنشاء الصورة*';
+    }
+  } catch (error) {
+    throw '*أُووبس! حدث خطأ ما أثناء إنشاء الصور. الرجاء معاودة المحاولة في وقت لاحق.*';
+  }
+};
+
+handler.help = ['photoleap'];
+handler.tags = ['drawing'];
+handler.command = ['ph'];
+export default handler;
