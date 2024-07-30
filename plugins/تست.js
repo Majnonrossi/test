ff8b4a224878de7fe7a42597e6d2f03c28bd845d
Majnon <162066183+Majnonrossi@users.noreply.github.com>
2024-07-30 16:09:22 +0100

    Update تست.js

--- plugins/تست.js
+++ plugins/تست.js
@@ -1,5 +1,4 @@
-import fetch from 'node-fetch';
-
+import axios from 'axios';
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `*هذا الأمر خاص بتوليد الصور باستعمال الذكاء الإصطناعي*\n\n*مثال*\n*${usedPrefix + command} boy*`;
 
@@ -7,10 +6,8 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     m.reply('*الرجاء الانتظار، جاري إنشاء الصور...*');
 
     const endpoint = `https://bk9.fun/ai/photoleap?q=Boy}`;
-    const response = await fetch(endpoint);
-
-    if (response.ok) {
-      const imageBuffer = await response.BK9.buffer();
+    const response = await axios.get(endpoint);
+      const imageBuffer = response.data.BK9;
       await conn.sendFile(m.chat, imageBuffer, 'image.png', null, m);
     } else {
       throw '*فشل إنشاء الصورة*';
@@ -22,5 +19,5 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.help = ['photoleap'];
 handler.tags = ['drawing'];
-handler.command = ['phot'];
+handler.command = ['photoleap'];
 export default handler;
